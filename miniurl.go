5910a6ae25ba53e81fefa81da6e883b17b09965b
Riku Kallio <riku.kallio@compile.fi>
2023-05-10 09:26:54 +0300

    feature: added Hash function

--- miniurl.go
+++ miniurl.go
@@ -1,1 +1,13 @@
 package miniurl
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// Returns Hashed output of long input
+func Hash(input string) string {
+	hash := md5.Sum([]byte(input))
+
+	return hex.EncodeToString(hash[:])
+}
