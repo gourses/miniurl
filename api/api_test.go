8ec4b43146991e23582d28a4d28ef8e86f82c370
Riku Kallio <riku.kallio@compile.fi>
2023-05-10 11:08:14 +0300

    feature: added barebone for API

---
+++ api/api_test.go
@@ -0,0 +1,36 @@
+package api_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/RichieRock/gourses-miniurl/api"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAPI_AddUrl(t *testing.T) {
+	const (
+		payload = `{"url": "https://github.com/gourses/miniurl/blob/main/LICENSE"}`
+		expectedBody = `{"url": "https://github.com/gourses/miniurl/blob/main/LICENSE", "hash": "testvalue"}`
+		expectedStatusCode = http.StatusOK
+	)
+
+	req := httptest.NewRequest(http.MethodPost, "/api/v1/url", strings.NewReader(payload))
+	rr := httptest.NewRecorder()
+
+	r := httprouter.New()
+	api.Bind(r, nil)
+	r.ServeHTTP(rr, req)
+
+	// check status code
+	assert.Equal(t, expectedStatusCode, rr.Result().StatusCode)
+	// read the body
+	body, err := io.ReadAll(rr.Result().Body)
+	require.NoError(t, err)
+	assert.Equal(t, expectedBody, body)
+}
